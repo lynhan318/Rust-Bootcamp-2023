812de53b034b4a38850ed88a8281839458abcf28
kevin <lythanhnhan27294@gmail.com>
2023-07-04 15:51:34 +0700

    feat: complete

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,25 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number == 0 {
+        "Zero"
+    } else if number > 0 {
+        "Positive"
+    } else {
+        "Negative"
+    }
 }
 
 // Exercise 3
@@ -23,7 +33,7 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -31,14 +41,17 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    match n {
+        0 => 1,
+        _ => (1..=n).product(),
+    }
 }
 
 // Exercise 6
@@ -46,9 +59,11 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    match n {
+        0 | 1 => false,
+        _ => (2..n).all(|i| n % i != 0),
+    }
 }
-
 
 // Don't mind this for now :)
 #[cfg(test)]
@@ -93,7 +108,7 @@ mod tests {
     // Test for exercise 3
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(foo_if_fizz("fuzz"), "bar")
+        assert_eq!(foo_if_fizz("fuzz"), "baz")
     }
 
     // Test for exercise 3
@@ -101,7 +116,6 @@ mod tests {
     fn default_to_baz() {
         assert_eq!(foo_if_fizz("literally anything"), "baz")
     }
-
     // Test for exercise 4
     #[test]
     fn test_leap_year() {
@@ -143,6 +157,4 @@ mod tests {
         assert_eq!(is_prime(10), false);
         assert_eq!(is_prime(15), false);
     }
-
-
 }
